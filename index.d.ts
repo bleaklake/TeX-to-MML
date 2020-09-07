348e1c273c66cc319f2bbaf609bdda0c5b5a82f0
CreateurCreatif <codermanager@protonmail.com>
2020-09-08 01:16:15 +0200

    first commit

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,11 @@
-declare module "AM-to-CHTML" {
+declare module "AM-to-MML" {
 
-    interface options {
+    /* interface options {
         width?: number,
         ex?: number,
         em?: number,
         fontURL?: string
-    }
+    } */
 
-    export default function AMToCHTML(str: string, opts?: options): string;
+    export default function AMToCHTML(str: string): string;
 }
